cfa9b9b0d9911d28835703e342de26a09601bde0
Mariana Machado <marimeltywork@outlook.com>
2024-02-03 21:23:25 -0300

    First function with JavaScript and changes in HTML/CSS pages

---
+++ functionsOperations.js
@@ -0,0 +1,10 @@
+var screnResult = document.getElementById('display-screen');
+
+
+function addNumberToScreen(number) {
+    screnResult.value = screnResult.value + number;
+    return screnResult.value;
+}
+
+
+
